4cdcd7907a2d7b39a83fabf87b1a1c7b96fc4e56
malibayram <malibayram91@gmail.com>
2024-02-06 13:30:38 +0300

    splash ekran eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'firebase_options.dart';
 import 'harun_odev_2.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterNativeSplash.remove();
 
   pozitifSayiTopla(a: 12, b: 5);
 
