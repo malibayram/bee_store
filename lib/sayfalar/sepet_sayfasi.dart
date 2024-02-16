26035cbdc4625e47d42fdd5353ea447da65c0858
malibayram <malibayram91@gmail.com>
2024-02-16 19:27:16 +0300

    sepet toplam tutarı hesaplayan widget eklendi

--- lib/sayfalar/sepet_sayfasi.dart
+++ lib/sayfalar/sepet_sayfasi.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bee_store/cubitler/sepet_cubit.dart';
 import 'package:bee_store/modeller/urun_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -13,30 +15,107 @@ class SepetSayfasi extends StatelessWidget {
       appBar: AppBar(title: const Text("Sepetim")),
       body: BlocBuilder<SepetCubit, List<String>>(
         builder: (_, state) {
+          if (state.isEmpty) {
+            return const Center(child: Text("Sepetiniz boş"));
+          }
+
+          final toplamSepetTutariNotifier = ValueNotifier(0.0);
+
           return Column(
             children: [
-              for (final urunID in state)
-                FutureBuilder(
-                  future: FirebaseFirestore.instance
-                      .collection('products')
-                      .doc(urunID)
-                      .get(),
-                  builder: (_, snapshot) {
-                    if (snapshot.hasData) {
-                      final urun = UrunModel.fromFirestore(
-                        snapshot.data!.data()!,
-                        snapshot.data!.id,
-                      );
-                      return Card(
-                        child: ListTile(
-                          title: Text(urun.baslik),
-                          subtitle: Text(urun.fiyatUSD.toString()),
+              Expanded(
+                child: ListView(
+                  children: [
+                    for (final urunID in state)
+                      FutureBuilder(
+                        future: FirebaseFirestore.instance
+                            .collection('products')
+                            .doc(urunID)
+                            .get(),
+                        builder: (_, snapshot) {
+                          if (snapshot.hasData) {
+                            final urun = UrunModel.fromFirestore(
+                              snapshot.data!.data()!,
+                              snapshot.data!.id,
+                            );
+
+                            final indirimliFiyat = urun.fiyatUSD -
+                                urun.fiyatUSD * urun.indirimOrani;
+
+                            Future.delayed(Duration(
+                                    milliseconds: Random().nextInt(150)))
+                                .whenComplete(() => toplamSepetTutariNotifier
+                                    .value += indirimliFiyat);
+
+                            return Card(
+                              child: ListTile(
+                                title: Text(urun.baslik),
+                                subtitle: Row(
+                                  children: [
+                                    Text("\$${urun.fiyatUSD}"),
+                                    const VerticalDivider(),
+                                    Text(
+                                      "İndirimli fiyat: \$${indirimliFiyat.toStringAsFixed(2)}",
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }
+                          return const LinearProgressIndicator();
+                        },
+                      ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Toplam sepet tutarı: "),
+                            ValueListenableBuilder(
+                              valueListenable: toplamSepetTutariNotifier,
+                              builder: (_, tutar, __) {
+                                return Text(
+                                  "\$${tutar.toStringAsFixed(2)}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      );
-                    }
-                    return const LinearProgressIndicator();
-                  },
-                )
+                        const Spacer(),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(
+                              Colors.blue,
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Ödemeye Geç",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           );
         },
--- lib/sayfalar/yuklenme_sayfasi.dart
+++ lib/sayfalar/yuklenme_sayfasi.dart
@@ -1,10 +1,9 @@
 import 'package:bee_store/cubitler/sepet_cubit.dart';
+import 'package:bee_store/sayfalar/anasayfa.dart';
 import 'package:bee_store/sayfalar/giris_sayfasi.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'anasayfa.dart';
 
 class YuklenmeSayfai extends StatelessWidget {
   const YuklenmeSayfai({super.key});
